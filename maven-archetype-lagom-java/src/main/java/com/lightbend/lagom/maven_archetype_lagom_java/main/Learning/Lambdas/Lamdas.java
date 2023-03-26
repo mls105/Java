2eb3c6e5df44e32fab077cdf769c7f7f2cc649fd
18603 <matthew.sextonusmc91@gmail.com>
2023-03-26 16:10:20 -0400

    added lambas folder

---
+++ maven-archetype-lagom-java/src/main/java/com/lightbend/lagom/maven_archetype_lagom_java/main/Learning/Lambdas/Lamdas.java
@@ -0,0 +1,11 @@
+package com.lightbend.lagom.maven_archetype_lagom_java.main.Learning.Lambdas;
+
+/**
+ * Class to show off what Lambdas can do
+ * 
+ * @author 18603
+ *
+ */
+public class Lamdas {
+
+}
