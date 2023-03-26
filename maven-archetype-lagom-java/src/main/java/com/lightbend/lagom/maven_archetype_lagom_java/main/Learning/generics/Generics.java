a78d56e22b8ad645af6928cb912ec684a0a507f5
18603 <matthew.sextonusmc91@gmail.com>
2023-03-26 16:07:41 -0400

    folder restructure

---
+++ maven-archetype-lagom-java/src/main/java/com/lightbend/lagom/maven_archetype_lagom_java/main/Learning/generics/Generics.java
@@ -0,0 +1,10 @@
+package com.lightbend.lagom.maven_archetype_lagom_java.main.Learning.generics;
+
+/**
+ * Class to show off what generics can do
+ * @author 18603
+ *
+ */
+public class Generics {
+
+}
