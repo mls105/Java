4b72f3d845c12919d13a647ceb885828f291060d
18603 <matthew.sextonusmc91@gmail.com>
2023-08-15 20:36:10 -0400

    updated folder and classes for unit testing

---
+++ maven-archetype-lagom-java/src/test/java/com/lightbend/lagom/maven_archetype_lagom_java/Learning/Lambdas/LambdasTest.java
@@ -0,0 +1,5 @@
+package com.lightbend.lagom.maven_archetype_lagom_java.Learning.Lambdas;
+
+public class LambdasTest {
+
+}
---
+++ maven-archetype-lagom-java/src/test/java/com/lightbend/lagom/maven_archetype_lagom_java/Learning/generics/GenericsTest.java
@@ -0,0 +1,5 @@
+package com.lightbend.lagom.maven_archetype_lagom_java.Learning.generics;
+
+public class GenericsTest {
+
+}
