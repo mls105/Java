9539e8be95835a3785d201d0fadbc5351170f604
18603 <matthew.sextonusmc91@gmail.com>
2023-11-20 21:56:39 -0500

    fixed unit testing getting broken by pom dependency

--- maven-archetype-lagom-java/src/test/java/com/lightbend/lagom/maven_archetype_lagom_java/main/InnerClassTest/InnerClassTest.java
+++ maven-archetype-lagom-java/src/test/java/com/lightbend/lagom/maven_archetype_lagom_java/main/InnerClassTest/InnerClassTest.java
@@ -1,5 +1,7 @@
 package com.lightbend.lagom.maven_archetype_lagom_java.main.InnerClassTest;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 
 import com.lightbend.lagom.maven_archetype_lagom_java.main.InnerClass.OuterClass;
@@ -10,6 +12,7 @@ public class InnerClassTest {
 	@Test
 	public void testInnerclass()
 	{
+		System.out.println("what is going on");
 		OuterClass outer = new OuterClass();
 		outer.heythere();
 		
@@ -17,6 +20,8 @@ public class InnerClassTest {
 //		OuterClass.InnerClass inner = outer.new InnerClass();
 		OuterClass.InnerClass inner = new OuterClass.InnerClass();
 		inner.whatsUp();
+		
+		assertNotNull(outer);
 				
 	}
 }
