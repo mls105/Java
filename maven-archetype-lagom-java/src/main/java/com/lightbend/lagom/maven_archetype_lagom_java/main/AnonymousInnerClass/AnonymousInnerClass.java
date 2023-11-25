d0396ae0ca66d7b3abf235d333932bf25880b876
18603 <matthew.sextonusmc91@gmail.com>
2023-11-25 11:52:34 -0500

    added unit testing to SO question

---
+++ maven-archetype-lagom-java/src/main/java/com/lightbend/lagom/maven_archetype_lagom_java/main/AnonymousInnerClass/AnonymousInnerClass.java
@@ -0,0 +1,6 @@
+package com.lightbend.lagom.maven_archetype_lagom_java.main.AnonymousInnerClass;
+
+public class AnonymousInnerClass {
+	
+
+}
--- maven-archetype-lagom-java/src/test/java/com/lightbend/lagom/maven_archetype_lagom_java/main/StackOverflowQuestionsTest/DateTest.java
+++ maven-archetype-lagom-java/src/test/java/com/lightbend/lagom/maven_archetype_lagom_java/main/StackOverflowQuestionsTest/DateTest.java
@@ -1,19 +1,25 @@
 package com.lightbend.lagom.maven_archetype_lagom_java.main.StackOverflowQuestionsTest;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.lightbend.lagom.maven_archetype_lagom_java.main.StackOverflowQuestions.Date;
 
 public class DateTest {
-	
-    @Test
-    public void dateTest()
-    {
 
+	@Rule
+	public final ExpectedException exception = ExpectedException.none();
 
-    	Date validDate = new Date(1900, 2, 30);
-    	Date invalidYear = new Date(1899, 1, 30);
-    	Date invalidMonth = new Date(1900, -1, 3);
-    	Date invalidDaye = new Date(1900, 8, 32);
-    }
+	@Test
+	public void dateTest() {
+
+		Date validDate = new Date(1900, 2, 30);
+		exception.expect(IllegalArgumentException.class);
+		Date invalidYear = new Date(1899, 1, 30);
+		exception.expect(IllegalArgumentException.class);
+		Date invalidMonth = new Date(1900, -1, 3);
+		exception.expect(IllegalArgumentException.class);
+		Date invalidDay = new Date(1900, 8, 32);
+	}
 }
