45212b38df9f5459146342ebd597a57a951e31b3
18603 <matthew.sextonusmc91@gmail.com>
2023-11-26 18:08:12 -0500

    SO code added for testing

--- maven-archetype-lagom-java/src/main/java/com/lightbend/lagom/maven_archetype_lagom_java/main/App.java
+++ maven-archetype-lagom-java/src/main/java/com/lightbend/lagom/maven_archetype_lagom_java/main/App.java
@@ -1,7 +1,5 @@
 package com.lightbend.lagom.maven_archetype_lagom_java.main;
 
-import com.lightbend.lagom.maven_archetype_lagom_java.main.StackOverflowQuestions.TabbedPaneExample;
-
 /**
  * Hello world!
  *
@@ -17,9 +15,20 @@ public class App
     	//TODO Make a test with the eclipse shortcuts that are awesome
         //eclipse shorcut type sysout (ctr + space + enter) for prtintln 
     	
-    	java.awt.EventQueue.invokeLater( () -> TabbedPaneExample.createAndShowGUI() );
+//    	java.awt.EventQueue.invokeLater( () -> TabbedPaneExample.createAndShowGUI() );
        
     }
     
 
 }
+//public class App {
+//    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+//        Scanner scanner = new Scanner(System.in);
+//        File file= new File("C:\\Users\\18603\\Downloads\\file_example_WAV_1MG.wav");
+//        AudioInputStream audioStream= AudioSystem.getAudioInputStream(file);
+//        Clip clip= AudioSystem.getClip();
+//        clip.open(audioStream);
+//        clip.start();
+//        String response= scanner.next();
+//    }
+//}
