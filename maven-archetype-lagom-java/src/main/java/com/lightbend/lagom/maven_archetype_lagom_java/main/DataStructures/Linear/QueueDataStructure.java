5f7a862420351c43f96f47f6b7840a0c0e833207
18603 <matthew.sextonusmc91@gmail.com>
2023-12-11 22:59:04 -0500

    added classes for the various data structures

---
+++ maven-archetype-lagom-java/src/main/java/com/lightbend/lagom/maven_archetype_lagom_java/main/DataStructures/Linear/QueueDataStructure.java
@@ -0,0 +1,5 @@
+package com.lightbend.lagom.maven_archetype_lagom_java.main.DataStructures.Linear;
+
+public class QueueDataStructure {
+
+}
---
+++ maven-archetype-lagom-java/src/main/java/com/lightbend/lagom/maven_archetype_lagom_java/main/DataStructures/NonLinear/TreeDataStructure.java
@@ -0,0 +1,5 @@
+package com.lightbend.lagom.maven_archetype_lagom_java.main.DataStructures.NonLinear;
+
+public class TreeDataStructure {
+
+}
