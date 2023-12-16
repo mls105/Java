55ef2ca51f7d315e3b62dc1f0444be0b2019f50e
18603 <matthew.sextonusmc91@gmail.com>
2023-12-16 00:42:04 -0500

    init setup - errors with slf4j and maven compilation

--- CodeWritingWorkspace/CodeApp/src/main/java/com/me/CodeApp/App.java
+++ CodeWritingWorkspace/CodeApp/src/main/java/com/me/CodeApp/App.java
@@ -1,13 +1,35 @@
 package com.me.CodeApp;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        launch(args);
     }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		BorderPane root = new BorderPane();
+		Scene scene = new Scene(root,400,400);
+//		scene.getStylesheets().add(getClass().getResource("application.css"));
+		
+		
+	}
+    
+ 
 }
+
+
